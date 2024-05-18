7b0162fceea02674b26759feab1edd201ae584aa
Vidya <31491170+0974201@users.noreply.github.com>
2024-05-18 02:35:16 +0200

    need to fix the json oop-
    panda will still be the placeholder in the meantime

--- js/whateverthisis.js
+++ js/whateverthisis.js
@@ -1,4 +1,4 @@
-async function get_projects(){
+async function get_projects() {
     try {
         const response = await fetch('https://raw.githubusercontent.com/0974201/website/main/projecten.json?token=GHSAT0AAAAAACOXYTUAFQYGOIIMSICRIHGGZSIALLA'); //github link gebruikt want cors
         const projecten = await response.json();
@@ -7,7 +7,7 @@ async function get_projects(){
 
         make_article(projecten)
 
-    } catch(error) {
+    } catch (error) {
         const err = document.querySelector("#content")
         const txt = document.createElement('p');
 
@@ -18,34 +18,45 @@ async function get_projects(){
     }
 }
 
-function make_article(obj){
-    const dingetje = document.querySelector("#content");
+function make_article(obj) {
+    const div = document.querySelector("#content");
     const projecten = obj.projecten;
     console.log(projecten);
 
-    for(const project of projecten){
+    for (const project of projecten) {
+        console.log(project);
         const art = document.createElement("article");
         art.id = "proj";
-        let title = document.createElement('h1');
-        let thumb = document.createElement('img');
-        let text = document.createElement('span');
-        let link = document.createElement('a');
+        const title = document.createElement('h1');
+        const thumb = document.createElement('img');
+        const languages = document.createElement('span');
+        const text = document.createElement('span');
+        const linktxt = document.createElement('b');
+        const url = document.createElement('a');
 
         title.innerHTML = project.naam;
         thumb.innerHTML = project.thumbnail;
-        //thumb.src = project.thumbnail;
         thumb.src = "https://upload.wikimedia.org/wikipedia/commons/6/67/Chengdu-pandas-d04.jpg";
-        text.innerHTML = project.tekst;
-        link.href = project.link;
-        link.target = "_blank";
-        link.innerHTML = "github";
-        
+        languages.innerHTML = "<b>Talen gebruikt: </b>" + project.talen + "<br/><br/>";
+        text.innerHTML = project.tekst + '<br/><br/>';
+        linktxt.innerHTML = "Link: "
+        url.href = project.url;
+        url.target = "_blank";
+
+        if (project.naam === 'Brews Clues') {
+            url.innerHTML = '<i class="fa-brands fa-itch-io"></i>' + " itch.io" + '<br/><br/>';
+        } else {
+            url.innerHTML = '<i class="fa-brands fa-github"></i>' + " github" + '<br/><br/>';
+        }
+
         art.appendChild(title);
         art.appendChild(thumb);
+        art.appendChild(languages);
         art.appendChild(text);
-        art.appendChild(link);
+        art.appendChild(linktxt);
+        art.appendChild(url);
 
-        dingetje.appendChild(art);
+        div.appendChild(art);
     }
 }
 
